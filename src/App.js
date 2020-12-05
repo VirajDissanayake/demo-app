2277220b3e40167dae854183f3267f7a91aca410
VirajDissanayake <41176872+VirajDissanayake@users.noreply.github.com>
2020-12-05 15:29:32 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {SignOut, withAuthenticator} from 'aws-amplify-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <SignOut/><br/>
+
+         <iframe src="https://search-eventlogsdemo-ihgnli4jp5xqcnl7mprjmfv7bq.us-east-1.es.amazonaws.com/_plugin/kibana/app/dashboards#/view/38364620-36df-11eb-9ec8-d31e29d50504?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now%2Fd,to:now%2Fd))&_a=(description:'',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:kuery,query:''),timeRestore:!f,title:logdashboard,viewMode:view)" height="900" width="100%"></iframe>
+
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+import '@aws-amplify/ui/dist/style.css';
+
+Amplify.configure(config)
 
 ReactDOM.render(
   <React.StrictMode>
